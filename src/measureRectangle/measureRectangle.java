6accd25137a1bca22cac03c29281b648dc528fda
akaur20 <42978513+akaur20@users.noreply.github.com>
2018-10-02 14:29:45 -0400

    Add files via upload

---
+++ src/measureRectangle/measureRectangle.java
@@ -0,0 +1,16 @@
+package measureRectangle;
+
+/*
+ * Avleen Kaur Period 7
+ */
+import java.util.Scanner;
+
+public class measureRectangle {
+
+	public static void main(String[] args) 
+	{
+		double sideLength = sideLength.nextDouble();
+
+	}
+
+}
