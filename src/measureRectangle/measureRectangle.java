257a624091a465a46b30749db01d7a140853f3c1
wizleen <wizleen@AVLEEN>
2018-10-03 06:44:40 -0400

    final

--- src/measureRectangle/measureRectangle.java
+++ src/measureRectangle/measureRectangle.java
@@ -9,8 +9,22 @@ public class measureRectangle {
 
 	public static void main(String[] args) 
 	{
-		double sideLength = sideLength.nextDouble();
-
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please enter the height of your rectangle:");
+		double height = in.nextDouble();
+		
+		System.out.print("Please enter the width of your rectangle:");
+		double width = in.nextDouble();
+		
+		double area = height*width;
+		double perimeter = height + height + width + width;
+		double diagonal = Math.hypot(height, width);
+		
+		System.out.printf("%.2f", area);
+		System.out.printf("%.2f", perimeter);
+		System.out.printf("%.2f", diagonal);
+		
 	}
 
 }
